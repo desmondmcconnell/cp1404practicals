23875c84b0846eb9a494f739705407f14a2d0fd0
Desmond <desmond.mcconnell@my.jcu.edu.au>
2018-08-30 14:41:09 +1000

    add word occurences program

---
+++ prac_05/word_occurences.py
@@ -0,0 +1,34 @@
+def main():
+    string = input("Please enter a string:")
+    print("Text: {}".format(string))
+    words = sorted(string.split(" "))
+    words_counts = generate_dictionary(words)
+    print_dictionary(words_counts, words)
+
+
+def generate_dictionary(words):
+    words_counts = {}
+    for word in words:
+        if word in words_counts:
+            words_counts[word] += 1
+        else:
+            words_counts[word] = 1
+    return words_counts
+
+
+def print_dictionary(words_counts, words):
+    longest_word = determine_longest_word(words)
+    for word in words_counts:
+        print("{:{}}: {}".format(word, longest_word, words_counts[word]))
+
+
+def determine_longest_word(words):
+    long_word = 0
+    for word in words:
+        if len(word) > long_word:
+            long_word = len(word)
+    return long_word
+
+
+main()
+
