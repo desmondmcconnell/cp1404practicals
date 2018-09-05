08d2ab28806fe13baf092c17537c94ed994188b6
Desmond <desmond.mcconnell@my.jcu.edu.au>
2018-09-06 00:17:46 +1000

    update guitars file with better methods

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,15 +1,13 @@
 from prac_06.guitar import Guitar
 
+TITLE = "My guitars!"
+
 
 def main():
-    print("My guitars!")
+    print(TITLE)
     guitars = []
     guitars = new_guitar(guitars)
-    print("These are my guitars:")
-    for i, guitar in enumerate(guitars):
-        vintage_string = (" (vintage)" if guitar.is_vintage() else "")
-        print("Guitar {}: {:>20} ({}), worth ${:10,.2f}{}".format(i + 1, guitar.name, guitar.year, guitar.cost,
-                                                                  vintage_string))
+    print_guitars(guitars)
 
 
 def new_guitar(guitars):
@@ -24,6 +22,14 @@ def new_guitar(guitars):
             guitar = Guitar(name, year, cost)
             guitars.append(guitar)
     return guitars
+
+
+def print_guitars(guitars):
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars):
+        vintage_string = (" (vintage)" if guitar.is_vintage( ) else "")
+        print("Guitar {}: {:>20} ({}), worth ${:10,.2f}{}".format(i + 1, guitar.name, guitar.year, guitar.cost,
+                                                                  vintage_string))
 
 
 main()
