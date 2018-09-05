fa99753ef1fa605f7f4794bb71ee49ff1fe93292
Desmond <desmond.mcconnell@my.jcu.edu.au>
2018-09-06 00:18:02 +1000

    update guitars file with better methods

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -27,7 +27,7 @@ def new_guitar(guitars):
 def print_guitars(guitars):
     print("These are my guitars:")
     for i, guitar in enumerate(guitars):
-        vintage_string = (" (vintage)" if guitar.is_vintage( ) else "")
+        vintage_string = (" (vintage)" if guitar.is_vintage() else "")
         print("Guitar {}: {:>20} ({}), worth ${:10,.2f}{}".format(i + 1, guitar.name, guitar.year, guitar.cost,
                                                                   vintage_string))
 
