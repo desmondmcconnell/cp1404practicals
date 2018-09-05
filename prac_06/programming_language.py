93941ff8e9729b51271ea131d2e0efd9d58b491c
Desmond <desmond.mcconnell@my.jcu.edu.au>
2018-09-05 22:31:34 +1000

    add prac_06 programming_languages and languages files

---
+++ prac_06/programming_language.py
@@ -0,0 +1,17 @@
+class ProgrammingLanguage:
+
+    def __init__(self, name="", typing="", reflection="", year=""):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        return "{self.name}, {self.typing} Typing, Reflection = {self.reflection}, " \
+               "First appeared in {self.year}".format(self=self)
+
+    def is_dynamic(self):
+        if self.typing == "Dynamic":
+            return True
+        return False
+
