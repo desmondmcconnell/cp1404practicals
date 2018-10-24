68e22b5c5188bb2fc27121c4f3f4b5b459ea9584
Desmond <desmond.mcconnell@my.jcu.edu.au>
2018-10-24 20:28:40 +1000

    add reverse recursion

--- prac_10/recursion.py
+++ prac_10/recursion.py
@@ -14,7 +14,7 @@ def do_it(n):
 # TODO: 1. write down what you think the output of this will be,
 # TODO: 2. use the debugger to step through and see what's actually happening
 print(do_it(5))
-
+print()
 
 def do_something(n):
     """Print the squares of positive numbers from n down to 0."""
@@ -24,10 +24,24 @@ def do_something(n):
         return
     do_something(n - 1)
 
+
 # TODO: 3. write down what you think the output of do_something(4) will be,
 # TODO: 4. use the debugger to step through and see what's actually happening
 
 
 do_something(4)
-
+print()
 # TODO: 5. fix do_something() so that it works according to the docstring
+
+
+def do_something_else(n):
+    """Print the squares of positive numbers from n down to 0."""
+    if n > 0:
+        do_something_else(n - 1)
+
+    else:
+        return
+    print(n ** 2)
+
+
+do_something_else(4)
